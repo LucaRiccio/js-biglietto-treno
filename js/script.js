1bc2ebd2e3e22a420078ad6ad28dede5e4c750aa
LucaRiccio <lucariccio888@gmail.com>
2020-07-07 17:40:51 +0200

    Esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
-var chilometri = prompt ("Chilometri da percorrere?")
-var eta = prompt ("Età del passeggero?")
+var chilometri = parseInt(prompt ("Chilometri da percorrere?"));
+var eta = parseInt(prompt ("Età del passeggero?"));
+var costo = 0.21 * chilometri;
+
+if (eta<18) {
+  document.getElementById('biglietto').innerHTML = "Costo del biglietto:" + costo * 0.8;
+}
+
+else if (eta>64) {
+  document.getElementById('biglietto').innerHTML = "Costo del biglietto:" + costo * 0.6;
+}
+
+else {
+  document.getElementById('biglietto').innerHTML = "Costo del biglietto:" + costo;
+}
