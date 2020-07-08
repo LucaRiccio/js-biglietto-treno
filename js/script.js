7e9514c349ec43eda9045445366ebcc193302091
LucaRiccio <lucariccio888@gmail.com>
2020-07-08 15:18:17 +0200

    Esercizio finito (revisione)

--- js/script.js
+++ js/script.js
@@ -3,13 +3,10 @@ var eta = parseInt(prompt ("Età del passeggero?"));
 var costo = 0.21 * chilometri;
 
 if (eta<18) {
-  document.getElementById('biglietto').innerHTML = "Costo del biglietto:" + costo * 0.8;
+  costo = costo * 0.8;
 }
-
 else if (eta>64) {
-  document.getElementById('biglietto').innerHTML = "Costo del biglietto:" + costo * 0.6;
+  costo = costo * 0.6;
 }
 
-else {
-  document.getElementById('biglietto').innerHTML = "Costo del biglietto:" + costo;
-}
+document.getElementById('biglietto').innerHTML = "Costo del biglietto:" + costo.toFixed(2) + " €";
